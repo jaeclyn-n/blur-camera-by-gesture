4e43889b11a06fea259b8ce9a1d3a42fb11de38b
Kaii <wtrayuga@gmail.com>
2026-06-22 21:56:08 +0700

    Clean project files

--- check_task.py
+++
@@ -1,4 +0,0 @@
-import mediapipe as mp
-
-print(mp.tasks)
-print(dir(mp.tasks))
--- test.py
+++
@@ -1,5 +0,0 @@
-import mediapipe as mp
-
-print("MediaPipe Version:", mp.__version__)
-print("MediaPipe Location:", mp.__file__)
-print(dir(mp))
